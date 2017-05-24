ee3201269e2636b8477bb66bd602a2ed6df451cd
heyrict <xiezh0831@126.com>
2017-05-24 20:46:15 +0800

    args replaced by dict along with UI improved in sysana.py

--- SysAna/sysana.py
+++ SysAna/sysana.py
@@ -1,14 +1,32 @@
 #!/usr/bin/env python3
 
 from exam import *
-from data_processing import split_wrd
+from data_processing import split_wrd, _in_list
 import pandas as pd
 
 class BeginQuestFormSysAna(BeginQuestForm):
+    def selchap(self,qf):
+        outqf = QuestForm()
+        kind = split_wrd(input('Which chapter to include?(empty to include all): '),list(', ，、'))
+        if kind:
+            outqf = outqf.append(QuestForm([i for i in qf if _in_list(','.join(i.q+i.sel),kind)]))
+            qf = outqf
+            outqf = QuestForm()
+        kind = InteractiveAnswer('Which difficulty(ies) to choose? ',\
+                serializer=lambda x:split_wrd(x,list(', ，、'),ignore_space=True),\
+                varify='1234').get()
+        outqf = outqf.append(QuestForm([i for i in qf if _in_list(i.args['Difficulty'],kind)]))
+        return outqf
+
     def raise_sel(self,quest):
         if quest.sel: 
             for s,t in zip(quest.sel,'ABCDE'):
                 print(t+':',s)
+
+    def raise_q(self,quest):
+        print('Question %d/%d: '%(self.correct+self.wrong+1,self.length),end='')
+        print('\n'.join(quest.q),'' if len(quest.ta[0])==1 else '[多选]')
+        return
 
     def check_ans(self,ans,quest):
         if set(list(split_wrd(ans.upper(),list(', ，、'),''))) == set(list(''.join(quest.ta))):
@@ -21,7 +39,8 @@ class BeginQuestFormSysAna(BeginQuestForm):
 
 def main():
     df = pd.read_excel('./Data.xlsx')
-    t=QuestFormExcelLoader(qcol='question',selcol=['option_'+i for i in 'abcde'],tacol='question_answer').load(df)
+    t=QuestFormExcelLoader(qcol='question',selcol=['option_'+i for i in 'abcde'],tacol='question_answer',\
+            argcol={'Difficulty':'question_difclt'}).load(df)
     BeginQuestFormSysAna(t).start()
 
 if __name__ == '__main__':
--- exam.py
+++ exam.py
@@ -8,7 +8,7 @@ from txtform import _space_fill
 BOARDER_LENGTH = 40
 
 class Quest():
-    def __init__(self,q,sel=None,ta=None,args=[]):
+    def __init__(self,q,sel=None,ta=None,args={}):
         self.q = q
         self.sel = sel
         self.ta = ta
@@ -24,12 +24,12 @@ class QuestForm(pd.Series):
 
 
 class QuestFormTextLoader():
-    def __init__(self,questpattern,qpattern,selpattern=None,tapattern=None,argpattern=[]):
+    def __init__(self,questpattern,qpattern,selpattern=None,tapattern=None,argpattern={}):
         self.questpattern = questpattern
         self.qpattern = qpattern
         self.selpattern = selpattern
         self.tapattern = tapattern
-        self.argpattern = [argpattern] if type(argpattern) == str else argpattern
+        self.argpattern = dict(argpattern)
 
     def get_cached_qf(self):
         if 'Curdata.data' in os.listdir():
@@ -43,14 +43,12 @@ class QuestFormTextLoader():
             qitem = re.findall(self.qpattern,quest)
             selitem = re.findall(self.selpattern,quest) if self.selpattern else None
             taitem = re.findall(self.tapattern,quest) if self.tapattern else None
-            argitem = [re.findall(pat,quest) for pat in self.argpattern] if self.argpattern else None
+            argitem = [(patnam,re.findall(self.argpattern(patnam),quest)) \
+                    for patnam in self.argpattern] if self.argpattern else None
             questform = questform.append(Quest(q=qitem,sel=selitem,ta=taitem,args=argitem))
         return questform
 
-    def load(self,queststr,args=None):
-        '''
-        args = [(argblockpattern,argnamepattern)+]
-        '''
+    def load(self,queststr):
         qf = self.get_cached_qf()
         if type(qf) != type(None): return qf
 
@@ -58,7 +56,7 @@ class QuestFormTextLoader():
 
 
 class QuestFormExcelLoader(QuestFormTextLoader):
-    def __init__(self,qcol,selcol=None,tacol=None,argcol=[]):
+    def __init__(self,qcol,selcol=None,tacol=None,argcol={}):
         super(QuestFormExcelLoader,self).__init__(None,qcol,selcol,tacol,argcol)
 
     def _load(self,questdf):
@@ -68,28 +66,28 @@ class QuestFormExcelLoader(QuestFormTextLoader):
             qitem = quest[self.qpattern]
             selitem = quest[self.selpattern] if self.selpattern else None
             taitem = quest[self.tapattern] if self.tapattern else None
-            argitem = [quest[pat] for pat in self.argpattern] if self.argpattern else None
+            argitem = {pat:quest[self.argpattern[pat]] for pat in self.argpattern} if self.argpattern else {}
 
-            qitem = [qitem] if type(qitem)==str else\
-                (None if type(qitem)==type(None) else list(qitem))
-            selitem = [selitem] if type(selitem)==str else\
-                (None if type(selitem)==type(None) else list(selitem))
-            taitem = [taitem] if type(taitem)==str else\
-                (None if type(taitem)==type(None) else list(taitem))
-            argitem = [argitem] if type(argitem)==str else\
-                (None if type(argitem)==type(None) else list(argitem))
+            qitem = [qitem] if type(qitem)==str else list(qitem)
+            selitem = [selitem] if type(selitem)==str else list(selitem)
+            taitem = [taitem] if type(taitem)==str  else list(taitem)
+
             questform = questform.append(Quest(q=qitem,sel=selitem,ta=taitem,args=argitem))
         return questform
 
 
 class BeginQuestForm():
-    def __init__(self,qf,arrange='qast'):
-        self.qf = qf
+    def __init__(self,qf,arrange='qast',no_score=False):
+        self.qf = self.selchap(qf)
         self.starttime = datetime.now()
         self.correct = self.wrong = 0
         self.length = len(self.qf)
         self.arrange = arrange
+        self.no_score = no_score
         self.arranged_index = list(range(self.length))
+
+    def selchap(self,qf):
+        return qf
 
     def oninit(self):
         if InteractiveAnswer('Randomize?',yes_or_no=True).get():
@@ -105,7 +103,8 @@ class BeginQuestForm():
         usedtime = relativedelta(datetime.now(),self.starttime)
         print(_space_fill('Total Time: %d hours, %d minutes, %d seconds'%(usedtime.hours,\
                         usedtime.minutes,usedtime.seconds),BOARDER_LENGTH))
-        if self.correct+self.wrong != 0:
+        if self.no_score: pass
+        elif self.correct+self.wrong != 0:
             print('Correct: ',self.correct)
             print('Wrong: ',self.wrong)
             print('Score: %.2f'%(self.correct/(self.correct+self.wrong)*100))
@@ -135,11 +134,8 @@ class BeginQuestForm():
                 self.raise_q(quest)
             elif re.findall('^'+a,'args'):
                 if not quest.args: continue
-                for arg in quest.args:
-                    if type(arg) == str:
-                        print(arg)
-                    else:
-                        print(': '.join([str(i) for i in arg]))
+                for k in quest.args:
+                    print(k+':',quest.args[k])
             elif re.findall('^'+a,'selection'):
                 self.raise_sel(quest)
             elif re.findall('^'+a,'true_answer'):
